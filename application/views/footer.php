f1104d649b223ffde5f5bc114b6d160d635a5978
yondan-ryz <yonathandani76@gmail.com>
2022-10-10 08:37:59 -0700

    g

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,24 +1,71 @@
+<footer class="site-footer">
+	<script src="<?=base_url()?>assets/js/core/jquery.min.js"></script>
+	<script src="<?=base_url()?>assets/js/core/popper.min.js"></script>
+	<script src="<?=base_url()?>assets/js/core/bootstrap.min.js"></script>
+	<script src="<?=base_url()?>assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+	<!--  Google Maps Plugin    -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+	<!-- Chart JS -->
+	<script src="<?=base_url()?>assets/js/plugins/chartjs.min.js"></script>
+	<!--  Notifications Plugin    -->
+	<script src="<?=base_url()?>assets/js/plugins/bootstrap-notify.js"></script>
+	<!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
+	<script src="<?=base_url()?>assets/js/paper-dashboard.min.js?v=2.0.1" type="text/javascript"></script><!-- Paper Dashboard DEMO methods, don't include it in your project! -->
+	<script src="<?=base_url()?>assets/demo/demo.js"></script>
+	<script>
+		$(document).ready(function() {
+			// Javascript method's body can be found in assets/assets-for-demo/js/demo.js
+			demo.initChartsPages();
+		});
+	</script>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-md-6">
+				<h6>About</h6>
+				<p class="text-justify">Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.</p>
+			</div>
 
-  <!--   Core JS Files   -->
-  <script src="<?=base_url()?>assets/js/core/jquery.min.js"></script>
-  <script src="<?=base_url()?>assets/js/core/popper.min.js"></script>
-  <script src="<?=base_url()?>assets/js/core/bootstrap.min.js"></script>
-  <script src="<?=base_url()?>assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-  <!--  Google Maps Plugin    -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-  <!-- Chart JS -->
-  <script src="<?=base_url()?>assets/js/plugins/chartjs.min.js"></script>
-  <!--  Notifications Plugin    -->
-  <script src="<?=base_url()?>assets/js/plugins/bootstrap-notify.js"></script>
-  <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="<?=base_url()?>assets/js/paper-dashboard.min.js?v=2.0.1" type="text/javascript"></script><!-- Paper Dashboard DEMO methods, don't include it in your project! -->
-  <script src="<?=base_url()?>assets/demo/demo.js"></script>
-  <script>
-    $(document).ready(function() {
-      // Javascript method's body can be found in assets/assets-for-demo/js/demo.js
-      demo.initChartsPages();
-    });
-  </script>
-</body>
+			<div class="col-xs-6 col-md-3">
+				<h6>Categories</h6>
+				<ul class="footer-links">
+					<li><a href="http://scanfcode.com/category/c-language/">C</a></li>
+					<li><a href="http://scanfcode.com/category/front-end-development/">UI Design</a></li>
+					<li><a href="http://scanfcode.com/category/back-end-development/">PHP</a></li>
+					<li><a href="http://scanfcode.com/category/java-programming-language/">Java</a></li>
+					<li><a href="http://scanfcode.com/category/android/">Android</a></li>
+					<li><a href="http://scanfcode.com/category/templates/">Templates</a></li>
+				</ul>
+			</div>
 
-</html>
+			<div class="col-xs-6 col-md-3">
+				<h6>Quick Links</h6>
+				<ul class="footer-links">
+					<li><a href="http://scanfcode.com/about/">About Us</a></li>
+					<li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+					<li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
+					<li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+					<li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+				</ul>
+			</div>
+		</div>
+		<hr>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-sm-6 col-xs-12">
+				<p class="copyright-text">Copyright &copy; 2017 All Rights Reserved by
+					<a href="#">Scanfcode</a>.
+				</p>
+			</div>
+
+			<div class="col-md-4 col-sm-6 col-xs-12">
+				<ul class="social-icons">
+					<li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+					<li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+					<li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+					<li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,5 +1,4 @@
-<footer>
-	<!-- Javascript files-->
+<footer class="site-footer">
 	<script src="<?= base_url('assets/js/jquery.min.js') ?>"></script>
 	<script src="<?= base_url('assets/js/popper.min.js') ?>"></script>
 	<script src="<?= base_url('assets/js/bootstrap.bundle.min.js') ?>"></script>
@@ -23,6 +22,57 @@
 				$(this).removeClass('transition');
 			});
 		});
-
 	</script>
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-6 col-md-3">
+				<h6>About Us</h6>
+				<ul class="footer-links">
+					<li><a class="" href="<?= base_url('/profil') ?>">Profil</a></li>
+					<li><a class="" href="#">Prestasi</a></li>
+					<li><a class="" href="<?= base_url('/beasiswa')?>">Beasiswa</a></li>
+				</ul>
+			</div>
+
+			<div class="col-xs-6 col-md-3">
+				<h6>Galeri</h6>
+				<ul class="footer-links">
+					<li><a class=""  href="<?= base_url('/fasilitas') ?>">Fasilitas</a></li>
+					<li><a class="" href="<?= base_url('/organisasi') ?>">Organisasi</a></li>
+					<li><a class="" href="<?= base_url('/ekstrakurikuler') ?>">Ekstrakulikuler</a></li>
+					<li><a class="" href="<?= base_url('/testimoni') ?>">Testimoni</a></li>
+				</ul>
+			</div>
+
+			<div class="col-xs-6 col-md-3">
+				<h6>Quick Links</h6>
+				<ul class="footer-links">
+					<li><a href="http://scanfcode.com/about/">About Us</a></li>
+					<li><a href="http://scanfcode.com/contact/">Contact Us</a></li>
+					<li><a href="http://scanfcode.com/contribute-at-scanfcode/">Contribute</a></li>
+					<li><a href="http://scanfcode.com/privacy-policy/">Privacy Policy</a></li>
+					<li><a href="http://scanfcode.com/sitemap/">Sitemap</a></li>
+				</ul>
+			</div>
+		</div>
+		<hr>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8 col-sm-6 col-xs-12">
+				<p class="copyright-text">Copyright &copy; 2022 All Rights Reserved by
+					<a href="https://ukrim.ac.id/">UKRIM University</a>.
+				</p>
+			</div>
+
+			<div class="col-md-4 col-sm-6 col-xs-12">
+				<ul class="social-icons">
+					<li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+					<li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+					<li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+					<li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
 </footer>
